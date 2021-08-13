8d8e1559d1c637b9d76559c6a0bf0f70a41404e9
paulomarcioalves <deso0152@algonquinlive.com>
2021-08-13 02:35:04 -0300

    creates-index

---
+++ final-assignment/script.js
@@ -0,0 +1,50 @@
+// image selector
+
+const imageSelectorItems = document.querySelectorAll('.image-selector-item')
+
+const homeImages = [
+    { id: 'still', path: './images/home/01-beer.jpg' },
+    { id: 'food', path: './images/home/02-milkshake.jpg' },
+    { id: 'retouching', path: './images/home/03-before_after.png' },
+]
+
+const changeSelectedItem = (element) => {
+    imageSelectorItems.forEach(item => {
+        item.classList.remove('selected')
+    })
+    element.classList.add('selected')
+}
+
+const changeHomeBackground = ({ target }) => {
+    const homeSelector = document.querySelector('#home')
+    const [ selectedImage ] = homeImages.filter(image => image.id === target.id)
+
+    homeSelector.style.backgroundImage = `url(${selectedImage.path})`
+    changeSelectedItem(target)
+}
+
+imageSelectorItems.forEach(item => {
+    item.addEventListener('mouseover', changeHomeBackground)
+})
+
+// menu toggle
+
+const menuSelector = document.querySelector('.menu-icons')
+const headerSelector = document.querySelector('.header')
+
+const toggleMenu = () => {
+    headerSelector.classList.toggle('open')
+}
+
+menuSelector.addEventListener('click', toggleMenu) 
+
+// home redirector
+
+const homeRedirector = ({ target }) => {
+    window.location.href = `${target.id}.html`
+}
+
+imageSelectorItems.forEach(item => {
+    item.addEventListener('click', homeRedirector)
+})
+
